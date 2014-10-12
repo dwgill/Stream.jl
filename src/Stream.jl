7224389212210066d8e53a2d5b2750766f325040
Daniel Gill <dan2025@gmail.com>
2014-10-12 16:11:54 -0500

    Tentative implementation of @stream.
    Currently it only supports nesting a stream of symbols and :call
    expressions. In the future it may or may not be worthwhile to add support
    for nesting in :macrocall expressions.

--- src/Stream.jl
+++ src/Stream.jl
@@ -1,5 +1,20 @@
 module Stream
+export @stream
 
-# package code goes here
+macro stream(ex, stream...)
+  prior_ex = ex
+  for elem in stream
+    if isa(elem, Symbol)
+      prior_ex = Expr(:call, elem, prior_ex)
 
-end # module
+    elseif isa(elem, Expr) && elem.head == :call
+      insert!(elem.args, 2, prior_ex)
+      prior_ex = elem
+
+    else
+      throw(ArgumentError("Stream element $(elem) is neither Symbol nor function call."))
+    end
+  end
+  return esc(prior_ex)
+end
+end
